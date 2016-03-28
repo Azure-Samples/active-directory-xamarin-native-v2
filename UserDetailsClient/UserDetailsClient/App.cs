99c108e3fea71ba4c4dc307554923dead87e07b9
Vittorio Bertocci <vibro@libero.it>
2016-03-28 09:29:04 -0700

    first commit

---
+++ UserDetailsClient/UserDetailsClient/App.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace UserDetailsClient
+{
+    public class App : Application
+    {
+        public static PublicClientApplication PCA = null;
+        public static string ClientID = "a7d8cef0-4145-49b2-a91d-95c54051fa3f";
+        public static string[] Scopes = { "User.Read" };
+        public static string Username = string.Empty;
+        public App()
+        {
+            PCA = new PublicClientApplication(ClientID);
+            MainPage = new NavigationPage(new UserDetailsClient.MainPage());        
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ UserDetailsClient/UserDetailsClient/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace UserDetailsClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public IPlatformParameters platformParameters { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            App.PCA.PlatformParameters = platformParameters;
+            // let's see if we have a user in our belly already
+            try
+            {
+                AuthenticationResult ar = await App.PCA.AcquireTokenSilentAsync(App.Scopes);
+                RefreshUserData(ar.Token);
+                btnSignInSignOut.Text = "Sign out";
+            }
+            catch
+            {
+                // doesn't matter, we go in interactive more
+                btnSignInSignOut.Text = "Sign in";
+            }
+        }
+        async void OnSignInSignOut(object sender, EventArgs e)
+        {
+            if (btnSignInSignOut.Text == "Sign in")
+            {
+                AuthenticationResult ar = await App.PCA.AcquireTokenAsync(App.Scopes);
+                RefreshUserData(ar.Token);
+                btnSignInSignOut.Text = "Sign out";
+            }
+            else
+            {
+                foreach (var user in App.PCA.Users)
+                {
+                    user.SignOut();
+                }
+                slUser.IsVisible = false;
+                btnSignInSignOut.Text = "Sign in";
+            }
+        }
+
+        public async void RefreshUserData(string token)
+        {
+            //get data from API
+            HttpClient client = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me");
+            message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
+            HttpResponseMessage response = await client.SendAsync(message);
+            string responseString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                JObject user = JObject.Parse(responseString);
+
+                slUser.IsVisible = true;
+                lblDisplayName.Text = "displayName  " + user["displayName"].ToString();
+                lblGivenName.Text = "givenName  " + user["givenName"].ToString();
+                lblId.Text = "id  " + user["id"].ToString();               
+                lblSurname.Text = "surname  " + user["surname"].ToString();
+                lblUserPrincipalName.Text = "userPrincipalName  " + user["userPrincipalName"].ToString();
+
+                // just in case
+                btnSignInSignOut.Text = "Sign out";
+
+               
+            }
+            else
+            {
+                DisplayAlert("Something went wrong with the API call", responseString, "Dismiss");
+            }
+        }
+    }
+
+    
+}

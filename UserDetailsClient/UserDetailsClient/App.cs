30aeaacabe87afe9553295f56bd497afcca490f5
Jean-Marc Prieur <jmprieur@microsoft.com>
2019-04-09 12:05:25 +0200

    Update to MSAL.NET 3.0.3-preview

--- UserDetailsClient/UserDetailsClient/App.cs
+++ UserDetailsClient/UserDetailsClient/App.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.Identity.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Identity.Client.AppConfig;
 using Xamarin.Forms;
 
 namespace UserDetailsClient
@@ -13,7 +8,7 @@ namespace UserDetailsClient
         public static IPublicClientApplication PCA = null;
 
         /// <summary>
-        /// The ClientID is the Application ID found in the portal (https://apps.dev.microsoft.com). 
+        /// The ClientID is the Application ID found in the portal (https://go.microsoft.com/fwlink/?linkid=2083908). 
         /// You can use the below id however if you create an app of your own you should replace the value here.
         /// </summary>
         public static string ClientID = "a7d8cef0-4145-49b2-a91d-95c54051fa3f";

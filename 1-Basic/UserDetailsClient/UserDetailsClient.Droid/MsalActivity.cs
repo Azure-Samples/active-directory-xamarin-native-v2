58f538ac69749d7911bdd8195b15ce35d784eb46
Tiago Brenck <v-tibre@microsoft.com>
2020-01-14 09:58:01 -0800

    Hardcoded sample appId so users can run the sample without needing manual steps

--- 1-Basic/UserDetailsClient/UserDetailsClient.Droid/MsalActivity.cs
+++ 1-Basic/UserDetailsClient/UserDetailsClient.Droid/MsalActivity.cs
@@ -17,7 +17,7 @@ namespace UserDetailsClient.Droid
     [IntentFilter(new[] { Intent.ActionView },
         Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
         DataHost = "auth",
-        DataScheme = "msal[ClientID]")]
+        DataScheme = "msala7d8cef0-4145-49b2-a91d-95c54051fa3f")]
     public class MsalActivity : BrowserTabActivity
     {
     }

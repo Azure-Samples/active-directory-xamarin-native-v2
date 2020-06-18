1f2b47f0b79c294863a8fbbfe2ef8d60a20b692e
Prateek Mahendrakar <Prateek.Mahendrakar@healthstatinc.com>
2020-06-18 17:45:18 -0400

    PackageReference changes Nuget updates

--- 1-Basic/UserDetailsClient/UserDetailsClient.iOS/AppDelegate.cs
+++ 1-Basic/UserDetailsClient/UserDetailsClient.iOS/AppDelegate.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿
 using Foundation;
-using UIKit;
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Client.Platforms.iOS;
+using UIKit;
 
 namespace UserDetailsClient.iOS
 {

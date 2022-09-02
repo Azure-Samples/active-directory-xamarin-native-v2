0a2caccc3c7af89b88e3cccf24838e8d9e4e7431
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2022-09-02 16:12:33 -0700

    Added Remapper to Android

--- 3-With-Intune MAM/IntuneClient/active-directory-xamarin-intune.Android/IntuneSampleApp.cs
+++ 3-With-Intune MAM/IntuneClient/active-directory-xamarin-intune.Android/IntuneSampleApp.cs
@@ -15,7 +15,7 @@ namespace active_directory_xamarin_intune.Droid
     /// Due to an issue with debugging the Xamarin bound MAM SDK the Debuggable = false attribute must be added to the Application in order to enable debugging.
     /// Without this attribute the application will crash when launched in Debug mode. Additional investigation is being performed to identify the root cause.
     /// </remarks>
-    [Application(Debuggable = false)]
+    // [Application(Debuggable = false)]
 #else
     [Application]
 #endif

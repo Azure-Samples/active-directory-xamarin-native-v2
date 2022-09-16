f75a944f154f45b6adc2591a259cee6750850859
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2022-09-16 14:31:07 -0700

    Intune downgraded to Forms 4.4. MAUI release Pkg

--- 3-With-Intune MAM/IntuneClient/active-directory-xamarin-intune.Android/IntuneMAMConnector.cs
+++ 3-With-Intune MAM/IntuneClient/active-directory-xamarin-intune.Android/IntuneMAMConnector.cs
@@ -2,12 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using activedirectoryxamarinintune;
+using Android.Util;
 using Microsoft.Identity.Client;
 using Microsoft.Intune.Mam.Client.App;
 using Microsoft.Intune.Mam.Policy;
 
+[assembly: Xamarin.Forms.Dependency(typeof(active_directory_xamarin_intune.Droid.IntuneMAMConnector))]
 namespace active_directory_xamarin_intune.Droid
 {
     public class IntuneMAMConnector : IIntuneMAMConnector
@@ -19,26 +22,34 @@ namespace active_directory_xamarin_intune.Droid
         /// <returns></returns>
         public async Task DoMAMRegisterAsync(IntuneAppProtectionPolicyRequiredException exProtection)
         {
+            Log.Info(GetType().Name, "***Begin DoMAMRegisterAsync");
+            Debug.WriteLine("***Begin DoMAMRegisterAsync");
             // reset the registered event
             PCAWrapper.MAMRegsiteredEvent.Reset();
 
             // Invoke compliance API on a different thread
             await Task.Run(() =>
-                                {
-                                    IMAMComplianceManager mgr = MAMComponents.Get<IMAMComplianceManager>();
-                                    mgr.RemediateCompliance(exProtection.Upn, exProtection.AccountUserId, exProtection.TenantId, exProtection.AuthorityUrl, false);
-                                }).ConfigureAwait(false);
+            {
+                Log.Info(GetType().Name, "Attempting to remediateCompliance.");
+                Debug.WriteLine("***DoMAMRegisterAsync->RemediateCompliance");
+                IMAMComplianceManager mgr = MAMComponents.Get<IMAMComplianceManager>();
+                mgr.RemediateCompliance(exProtection.Upn, exProtection.AccountUserId, exProtection.TenantId, exProtection.AuthorityUrl, true);
+            }).ConfigureAwait(false);
 
 
             // wait till the registration completes
             // Note: This is a sample app for MSAL.NET. Scenarios such as what if enrollment fails or user chooses not to enroll will be as
             // per the business requirements of the app and not considered in the sample app.
+            Debug.WriteLine("***DoMAMRegisterAsync->Pre-Wait");
+            Log.Info(GetType().Name, "***DoMAMRegisterAsync->Pre-Wait");
             PCAWrapper.MAMRegsiteredEvent.WaitOne();
+            Debug.WriteLine("***DoMAMRegisterAsync->Post-Wait");
+            Log.Info(GetType().Name, "***DoMAMRegisterAsync->Post-Wait");
         }
 
         public void Unenroll()
         {
-            // Not implpemented on Android.
+            // Not implpemented on Android by Broker. Known issue.
         }
     }
 }

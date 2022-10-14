5da13abfe0b2600c24dbc1b2a56a294fcbf8568e
Michael Mitchell-Halter <v-michaelmi@microsoft.com>
2022-10-14 11:09:25 -0700

    Added missing code and removed 'MainViewModel'

--- MAUI/MauiAppB2C/AppShell.xaml.cs
+++ MAUI/MauiAppB2C/AppShell.xaml.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using MauiB2C.Views;
+
 namespace MauiB2C;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public AppShell()
+    {
+        InitializeComponent();
+        Routing.RegisterRoute("mainview", typeof(MainView));
+        Routing.RegisterRoute("scopeview", typeof(ScopeView));
+    }
 }

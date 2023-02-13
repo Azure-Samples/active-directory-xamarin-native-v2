50e167460ec9cd115ce7dcd2df1c999bf8aa1f68
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2023-02-13 12:06:39 -0800

    Restoring MAUI samples for 4.50

---
+++ MAUI/MauiAppBasic/MSALClient/AppConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBasic.MSALClient
+{
+    /// <summary>
+    /// Defines constants for the app.
+    /// Please change them for your app
+    /// </summary>
+    internal class AppConstants
+    {
+        // ClientID of the application in (sample-testing.com)
+        internal const string ClientId = "4b706872-7c33-43f0-9325-55bf81d39b93"; // TODO - Replace with your client Id. And also replace in the AndroidManifest.xml
+
+        /// <summary>
+        /// Scopes defining what app can access in the graph
+        /// </summary>
+        internal static string[] Scopes = { "User.Read" };
+
+
+    }
+}
---
+++ MAUI/MauiAppBasic/MSALClient/PCAWrapper.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Abstractions;
+
+namespace MauiAppBasic.MSALClient
+{
+    /// <summary>
+    /// This is a wrapper for PublicClientApplication. It is singleton.
+    /// </summary>
+    public class PCAWrapper
+    {
+        /// <summary>
+        /// This is the singleton used by Ux. Since PCAWrapper constructor does not have perf or memory issue, it is instantiated directly.
+        /// </summary>
+        public static PCAWrapper Instance { get; private set; } = new PCAWrapper();
+
+        /// <summary>
+        /// Instance of PublicClientApplication. It is provided, if App wants more customization.
+        /// </summary>
+        internal IPublicClientApplication PCA { get; }
+
+        /// <summary>
+        /// This will determine if the Interactive Authentication should be Embedded or System view
+        /// </summary>
+        internal bool UseEmbedded { get; set; } = false;
+
+        // private constructor for singleton
+        private PCAWrapper()
+        {
+            // Create PCA once. Make sure that all the config parameters below are passed
+            PCA = PublicClientApplicationBuilder
+                                        .Create(AppConstants.ClientId)
+                                        .WithExperimentalFeatures() // this is for upcoming logger
+                                        .WithLogging(_logger)
+                                        .WithRedirectUri(PlatformConfig.Instance.RedirectUri)
+                                        .WithIosKeychainSecurityGroup("com.microsoft.adalcache")
+                                        .Build();
+        }
+
+        /// <summary>
+        /// Acquire the token silently
+        /// </summary>
+        /// <param name="scopes">desired scopes</param>
+        /// <returns>Authentication result</returns>
+        internal async Task<AuthenticationResult> AcquireTokenSilentAsync(string[] scopes)
+        {
+            var accts = await PCA.GetAccountsAsync().ConfigureAwait(false);
+            var acct = accts.FirstOrDefault();
+
+            var authResult = await PCA.AcquireTokenSilent(scopes, acct)
+                                        .ExecuteAsync().ConfigureAwait(false);
+            return authResult;
+
+        }
+
+        /// <summary>
+        /// Perform the interactive acquisition of the token for the given scope
+        /// </summary>
+        /// <param name="scopes">desired scopes</param>
+        /// <returns></returns>
+        internal async Task<AuthenticationResult> AcquireTokenInteractiveAsync(string[] scopes)
+        {
+            if (UseEmbedded)
+            {
+
+                return await PCA.AcquireTokenInteractive(scopes)
+                                        .WithUseEmbeddedWebView(true)
+                                        .WithParentActivityOrWindow(PlatformConfig.Instance.ParentWindow)
+                                        .ExecuteAsync()
+                                        .ConfigureAwait(false);
+            }
+
+            SystemWebViewOptions systemWebViewOptions = new SystemWebViewOptions();
+#if IOS
+            // Hide the privacy prompt in iOS
+            systemWebViewOptions.iOSHidePrivacyPrompt = true;
+#endif
+
+            return await PCA.AcquireTokenInteractive(scopes)
+                                    .WithSystemWebViewOptions(systemWebViewOptions)
+                                    .WithParentActivityOrWindow(PlatformConfig.Instance.ParentWindow)
+                                    .ExecuteAsync()
+                                    .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// It will sign out the user.
+        /// </summary>
+        /// <returns></returns>
+        internal async Task SignOutAsync()
+        {
+            var accounts = await PCA.GetAccountsAsync().ConfigureAwait(false);
+            foreach (var acct in accounts)
+            {
+                await PCA.RemoveAsync(acct).ConfigureAwait(false);
+            }
+        }
+
+        // Custom logger for sample
+        private readonly MyLogger _logger = new MyLogger();
+
+        // Custom logger class
+        private class MyLogger : IIdentityLogger
+        {
+            /// <summary>
+            /// Checks if log is enabled or not based on the Entry level
+            /// </summary>
+            /// <param name="eventLogLevel"></param>
+            /// <returns></returns>
+            public bool IsEnabled(EventLogLevel eventLogLevel)
+            {
+                //Try to pull the log level from an environment variable
+                var msalEnvLogLevel = Environment.GetEnvironmentVariable("MSAL_LOG_LEVEL");
+
+                EventLogLevel envLogLevel = EventLogLevel.Informational;
+                Enum.TryParse<EventLogLevel>(msalEnvLogLevel, out envLogLevel);
+
+                return envLogLevel <= eventLogLevel;
+            }
+
+            /// <summary>
+            /// Log to console for demo purpose
+            /// </summary>
+            /// <param name="entry">Log Entry values</param>
+            public void Log(LogEntry entry)
+            {
+                Console.WriteLine(entry.Message);
+            }
+        }
+    }
+}
